0e6ad7a53ebc7ddb434cfa633707f3cbb3123db9
rpadebet <rohit.pittu@gmail.com>
2017-05-14 00:27:59 -0400

    Excel Regression Model

--- Regression Model/MTCarsProject.R
+++ Regression Model/MTCarsProject.R
@@ -8,6 +8,28 @@ head(mtcars)
 str(mtcars)
 summary(mtcars)
 
+# Description
+#' The data was extracted from the 1974 Motor Trend US magazine, 
+#' and comprises fuel consumption and 10 aspects of automobile design and 
+#' performance for 32 automobiles (1973–74 models).
+#' 
+#' 
+#' Data Model
+#' 
+#' A data frame with 32 observations on 11 variables.
+
+# [, 1]	mpg	Miles/(US) gallon
+# [, 2]	cyl	Number of cylinders
+# [, 3]	disp	Displacement (cu.in.)
+# [, 4]	hp	Gross horsepower
+# [, 5]	drat	Rear axle ratio
+# [, 6]	wt	Weight (1000 lbs)
+# [, 7]	qsec	1/4 mile time
+# [, 8]	vs	V/S
+# [, 9]	am	Transmission (0 = automatic, 1 = manual)
+# [,10]	gear	Number of forward gears
+# [,11]	carb	Number of carburetors
+
 # Factoring and labelling some of the data
  mtcars$cyl <-as.factor(mtcars$cyl)
  mtcars$am <-as.factor(mtcars$am)
