a567d131168b7d774fdb5667e7328887f8edf2df
rpadebet <rohit.pittu@gmail.com>
2017-07-31 04:24:13 -0400

    Made changes to spark code to include H2o on cluster

--- Week 5/AWS Spark Flights.R
+++ Week 5/AWS Spark Flights.R
@@ -2,6 +2,8 @@
 library(devtools)
 remove.packages("h2o")
 install_version("h2o","3.10.5.2")
+install.packages("h2o", type="source", repos="http://h2o-release.s3.amazonaws.com/h2o/rel-vajda/4/R")
+
 #install_version("rsparkling","0.2.1")
 
 library(sparklyr)
@@ -15,11 +17,11 @@ library(ggthemes)
 Sys.setenv(SPARK_HOME="/usr/lib/spark") # on aws cluster
 
 config <- list(
-    "spark.dynamicAllocation.enabled" = "false"
-    "sparklyr.shell.driver-memory"= "4G",
-    "sparklyr.shell.num-executors" = 2,
-    "sparklyr.shell.executor-memory" = "4G",
-    "sparklyr.shell.executor-cores" = 3
+    "spark.dynamicAllocation.enabled" = "false",
+    "sparklyr.shell.driver-memory"= "6G",
+    "sparklyr.shell.num-executors" = 3,
+    "sparklyr.shell.executor-memory" = "8G",
+    "sparklyr.shell.executor-cores" = 4
 )
 
 sc <- spark_connect(master = "yarn-client", config = config) # on aws cluster
@@ -47,7 +49,7 @@ airlines_spark <- tbl(sc, 'airlines_hive')
 tbl_cache(sc, 'airports_hive')
 airports_spark <- tbl(sc, 'airports_hive')
 
-test<-as_h2o_frame(sc,airlines_spark,strict_version_check = F)
+test<-as_h2o_frame(sc,airlines_spark)
 
 # Perform the best worst analysis
 # Perform the best worst analysis again inside our Postgres database
@@ -84,10 +86,10 @@ system.time(collect(flights_bestworst.spark))
 
 
 model_data_spk <- flights_spark %>%
-    filter(!is.na(arrdelay) & !is.na(depdelay) & !is.na(distance))%>%
+    filter(!is.na(arrdelay) & !is.na(depdelay) & !is.na(distance) & !is.na(airtime) & airtime>0)%>%
     filter(year >=1987 & year<=2007)%>%
     mutate(gain = depdelay - arrdelay)%>%
-    mutate(speed = distance/(crsarrtime-crsdeptime))%>%
+    mutate(speed = distance/(airtime))%>%
     select(year, month, arrdelay, depdelay, distance, uniquecarrier, origin,dest, gain,speed)%>%
     left_join(airlines_spark, by = c('uniquecarrier' = 'code'))%>%  
     inner_join(y=airports_spark, by = c('origin'='faa'))%>%  
@@ -106,11 +108,11 @@ glimpse(model_data_spk)
 ##                    H2O Machine Learning
 ###########################################################
 
-h2o.init(port = 54321,nthreads = -1)
+#h2o.init(port = 54321,nthreads = -1)
 
 # Push the data into h2o
 model_data_spk_h2o<-as_h2o_frame(sc,model_data_spk)
-model_data_spk_h2o<-as.h2o(model_data_spk)
+#model_data_spk_h2o<-as.h2o(model_data_spk)
 model_data_spk_h2o$uniquecarrier<-as.factor(model_data_spk_h2o$uniquecarrier)
 
 # Split the data
@@ -135,44 +137,7 @@ predictions_train<-h2o.predict(model_spk,data_spk$train)
 # Predict on test set
 predictions_test<-h2o.predict(model_spk,newdata = data_spk$test)
 
-
-# Combining the predictions with the data and merging the frames
-data_spk$train$prediction<-predictions_train
-data_spk$train$data<-"train"
-data_spk$test$prediction<-predictions_test
-data_spk$test$data<-"test"
-
-data_h2o<-h2o.rbind(data_spk$train,data_spk$test)
-
-# Convert to tbl to process in plots (Now we need to get into R)
-model_data_pred_spk<-as_tibble(data_h2o)
-
-# Calculate average gains by predicted decile
-model_deciles <- model_data_pred_spk%>%
-    mutate(decile = ntile(desc(prediction),10))%>%
-    group_by(decile,data) %>%
-    summarize(gain = mean(gain)) %>%
-    select(decile, gain,data)
-
-# Plot average gains by predicted decile
-
-model_deciles %>%
-    ggplot(aes(factor(decile), gain, fill = data)) +
-    geom_bar(stat = 'identity', position = 'dodge') +
-    labs(title = 'Average gain by predicted decile', x = 'Decile', y = 'Minutes')+
-    theme_wsj()
-
-# Summarize data by carrier
-carrier<-model_data_pred_spk%>%
-    group_by(airline_name)%>%
-    summarize(gain = mean(gain), prediction = mean(prediction), freq = n())%>%
-    filter(freq>10000)
+# Download the model
+h2o.saveModel(model_spk,path = "./",force = T)
 
 
-# Plot actual gains and predicted gains by airline carrier
-ggplot(carrier, aes(gain, prediction)) + 
-    geom_point(alpha = 0.75, color = 'red', shape = 3) +
-    geom_abline(intercept = 0, slope = 1, alpha = 0.15, color = 'blue') +
-    geom_text(aes(label = substr(airline_name, 1, 20)), size = 3, alpha = 0.75, vjust = -1) +
-    labs(title='Average Gains Forecast', x = 'Actual', y = 'Predicted')+
-    theme_economist()
